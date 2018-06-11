49c2fd59a27679216ced1b24c87bc91dcc7dc925
wm <675950896@qq.com>
2018-06-11 20:24:47 +0800

    eight

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
-class UserControllerTest < ActionController::TestCase
+class UsersControllerTest < ActionController::TestCase
+
   test "should get new" do
     get :new
     assert_response :success
   end
-
 end
